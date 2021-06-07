58babad3a01fcae36b4136359ee31256f107ea6f
yousefsul <67277838+yousefsul@users.noreply.github.com>
2021-06-07 13:02:39 +0300

    Complete SplashScreen Functionality

---
+++ lib/pinPage/PinPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PinPage extends StatefulWidget {
+  const PinPage({Key? key}) : super(key: key);
+
+  @override
+  _PinPageState createState() => _PinPageState();
+}
+
+class _PinPageState extends State<PinPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('HomePage'),
+    );
+  }
+}
